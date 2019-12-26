64bec260b42ab55667c6bb66bd7d3cb62f1529ce
Berbardo <bernardo.rcoutinho@gmail.com>
2019-12-26 18:59:38 -0300

    Initial Commit

---
+++ inc/vl53l1x.h
@@ -0,0 +1,78 @@
+/**
+ * @file vl53l1x.h
+ *
+ * @brief User functions to deal with ST's VL53L1 API
+ *
+ * @author Lucas Schneider <lucas.schneider@thunderatz.org>
+ * @author Bernardo Coutinho <bernardo.coutinho@thunderatz.org>
+ *
+ * @date 12/2019
+ */
+
+#ifndef __VL53L1X_H__
+#define __VL53L1X_H__
+
+#include "vl53l1_api.h"
+#include "vl53l1_platform.h"
+
+/*****************************************
+ * Public Constants
+ *****************************************/
+
+#define VL53L1_DEFAULT_ADDRESS 152
+
+/*****************************************
+ * Public Functions Prototypes
+ *****************************************/
+
+/**
+ * @brief Initializes VL53L1 device
+ *
+ * @note Before initialization, check if the device can communicate with @ref vl53l1_wait_boot
+ *
+ * @param p_device      Device handler
+ * @param device_info   Device informations
+ * @param calibration   Device calibration data
+ *
+ * @return Error code
+ * @retval VL53L1_ERROR_NONE   Success
+ * @retval "Other error code"   More details in @ref VL53L1_Error
+ */
+VL53L1_Error vl53l1_init(VL53L1_Dev_t* p_device, VL53L1_DeviceInfo_t device_info,
+                           VL53L1_CalibrationData_t calibration);
+
+/**
+ * @brief Turns off device with the XSHUT pin
+ *
+ * @param p_device      Device handler
+ */
+void vl53l1_turn_off(VL53L1_Dev_t* p_device);
+
+/**
+ * @brief Turns on device with the XSHUT pin
+ *
+ * @param p_device      Device handler
+ */
+void vl53l1_turn_on(VL53L1_Dev_t* p_device);
+
+/**
+ * @brief Update sensor range
+ *
+ * @param p_device          Device handler
+ * @param p_ranging_data    Device measurement data
+ * @param range             Adress to store the measurement in mm
+ * @param max_range         Max range the sensor can read in mm
+ *
+ * @return Error code
+ * @retval 0                        Success
+ * @retval "Number between 1 and 5" Range status from sensor reading @see API_User_Manual
+ * @retval "Other error code"       More details in @ref VL53L1_Error
+ */
+uint8_t vl53l1_update_range(VL53L1_Dev_t* p_device, VL53L1_RangingMeasurementData_t* p_ranging_data, uint16_t* range,
+                             uint16_t max_range);
+
+uint8_t check_API_status();  // NOT IMPLEMENTED YET
+
+uint8_t vl53l1_reinit();  // NOT IMPLEMENTED YET
+
+#endif // __VL53L1X_H__
---
+++ src/vl53l1x.c
@@ -0,0 +1,184 @@
+/**
+ * @file vl53l1.c
+ *
+ * @brief User functions to deal with ST's VL53L1 API
+ *
+ * @author Lucas Schneider <lucas.schneider@thunderatz.org>
+ * @author Bernardo Coutinho <bernardo.coutinho@thunderatz.org>
+ *
+ * @date 10/2019
+ */
+
+#include "vl53l1.h"
+#include "gpio.h"
+
+/*****************************************
+ * Private Macro
+ *****************************************/
+
+/**
+ * @brief Returns minimum value between x and y.
+ */
+#define min(x, y) ((x) < (y) ? (x) : (y))
+
+/*****************************************
+ * Private Constants
+ *****************************************/
+
+#define DISTANCEMODE VL53L1_DISTANCEMODE_MEDIUM
+#define SIGMA_LIMIT_VALUE_MM 18
+#define SIGNAL_RATE_LIMIT_VALUE_MCPS 0.25
+#define MEASUREMENT_TIME_BUDGET_US 50000
+
+/**
+ * @brief Filter values, depending on range status
+ */
+#define VALID_RANGE_FILTER 0.8
+#define SIGMA_FAIL_FILTER 0.2
+#define DAFAULT_FILTER 0.4
+
+/*****************************************
+ * Public Functions Bodies Definitions
+ *****************************************/
+
+VL53L1_Error vl53l1_init(VL53L1_Dev_t* p_device, VL53L1_DeviceInfo_t device_info,
+                           VL53L1_CalibrationData_t* calibration) {
+    VL53L1_Error Status = VL53L1_ERROR_NONE;
+
+    // Wait Device Booted
+    VL53L1_WaitDeviceBooted(p_device);
+
+    // Set I2c Address
+    Status = VL53L1_SetDeviceAddress(p_device, p_device->I2cDevAddr);
+
+    // Data initialization
+    Status = VL53L1_DataInit(p_device);
+
+    if (Status == VL53L1_ERROR_NONE) {
+        Status = VL53L1_GetDeviceInfo(p_device, &device_info);
+
+        if (Status == VL53L1_ERROR_NONE) {
+            if ((device_info.ProductRevisionMinor != 1) && (device_info.ProductRevisionMinor != 1)) {
+                Status = VL53L1_ERROR_NOT_SUPPORTED;
+            }
+        }
+    }
+
+    if (Status == VL53L1_ERROR_NONE) {
+        // Device Initialization
+        Status = VL53L1_StaticInit(p_device);
+    }
+
+    if (p_device->calibrated) {
+        // Load calibration data
+        if (Status == VL53L1_ERROR_NONE) {
+            Status = VL53L1_SetCalibrationData(p_device, calibration);
+        }
+
+    } else {
+        // Device calibration
+        if (Status == VL53L1_ERROR_NONE) {
+            Status = VL53L1_PerformRefSpadManagement(p_device);
+        }
+        
+        // Saving device calibration data
+        if (Status == VL53L1_ERROR_NONE) {
+            Status = VL53L1_GetCalibrationData(p_device, calibration);
+        }
+    }
+
+    if (Status == VL53L1_ERROR_NONE) {
+        // Set Distance Mode as Medium
+        Status = VL53L1_SetDistanceMode(p_device, DISTANCEMODE);
+    }
+
+    // Enable Sigma Signal and Threshold check
+    if (Status == VL53L1_ERROR_NONE) {
+        Status = VL53L1_SetLimitCheckEnable(p_device,
+                                             VL53L1_CHECKENABLE_SIGMA_FINAL_RANGE, 1);
+    }
+
+    if (Status == VL53L1_ERROR_NONE) {
+        Status = VL53L1_SetLimitCheckEnable(p_device,
+                                             VL53L1_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, 1);
+    }
+
+    // Set Values
+    if (Status == VL53L1_ERROR_NONE) {
+        // Aumentar Sigma -> Aumenta a range
+        Status = VL53L1_SetLimitCheckValue(p_device,
+                                            VL53L1_CHECKENABLE_SIGMA_FINAL_RANGE,
+                                            (FixPoint1616_t) (SIGMA_LIMIT_VALUE_MM * (1 << 16)));
+    }
+
+    if (Status == VL53L1_ERROR_NONE) {
+        // Diminuir Signal Rate -> Aumenta a range
+        Status = VL53L1_SetLimitCheckValue(p_device,
+                                            VL53L1_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE,
+                                            (FixPoint1616_t) (SIGNAL_RATE_LIMIT_VALUE_MCPS * (1 << 16)));
+    }
+
+    // VL53L1_UserRoi_t roiConfig;
+    // roiConfig.TopLeftX = 0;
+    // roiConfig.TopLeftY = 15;
+    // roiConfig.BotRightX = 15;
+    // roiConfig.BotRightY = 0;
+    // if (Status == VL53L1_ERROR_NONE) {
+    //     // Set Region of Interest (deixar padrão sla)
+    //     Status = VL53L1_SetUserROI(p_device, &roiConfig);
+    // }
+
+    if (Status == VL53L1_ERROR_NONE) {
+        Status = VL53L1_SetMeasurementTimingBudgetMicroSeconds(p_device, MEASUREMENT_TIME_BUDGET_US);
+    }
+
+    // Start reading
+    if (Status == VL53L1_ERROR_NONE) {
+        Status = VL53L1_StartMeasurement(p_device);
+    }
+
+    return Status;
+}
+
+void vl53l1_turn_off(VL53L1_Dev_t* p_device) {
+    HAL_GPIO_WritePin(p_device->xshut_port, p_device->xshut_pin, GPIO_PIN_RESET);
+}
+
+void vl53l1_turn_on(VL53L1_Dev_t* p_device) {
+    HAL_GPIO_WritePin(p_device->xshut_port, p_device->xshut_pin, GPIO_PIN_SET);
+}
+
+uint8_t vl53l1_update_range(VL53L1_Dev_t* p_device, VL53L1_RangingMeasurementData_t* p_ranging_data, uint16_t* range,
+                             uint16_t max_range) {
+    uint8_t status = 0;
+    uint16_t aux_range = *range;
+    status = VL53L1_GetRangingMeasurementData(p_device, p_ranging_data);
+
+    if (status != VL53L1_ERROR_NONE) {
+        return status;
+    }
+
+    uint8_t range_status = p_ranging_data->RangeStatus;
+
+    if (range_status == 0) {  // VALID RANGE
+        aux_range = (p_ranging_data->RangeMilliMeter) * VALID_RANGE_FILTER + (1 - VALID_RANGE_FILTER) * aux_range;
+    } else if (range_status == 1) {  // SIGMA FAIL
+        aux_range = (p_ranging_data->RangeMilliMeter) * SIGMA_FAIL_FILTER + (1 - SIGMA_FAIL_FILTER) * aux_range;
+    } else if (range_status == 4) {  // PHASE FAIL
+        /* In this case, aux_range will not be updated, because the reading is mostly random when this erro occurs */
+    } else if (range_status == 5) {  // HARDWARE FAIL
+        aux_range = max_range;
+    } else {
+        // 2 - SIGNAL FAIL
+        // 3 - MIN RANGE FAIL
+        aux_range = (p_ranging_data->RangeMilliMeter) * DAFAULT_FILTER + (1 - DAFAULT_FILTER) * aux_range;
+    }
+
+    *range = min(aux_range, max_range);
+
+    if (status == 0) {
+        status = range_status;
+    }
+
+    return status;
+}

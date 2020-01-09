ba5d979bb62147e324e1ce833b20d720255970fe
Lucas Haug <lucas.haug@hotmail.com>
2020-01-09 17:55:38 -0300

    Add tests

--- src/main.c
+++ src/main.c
@@ -8,11 +8,13 @@
  * @date 12/2019
  */
 
+#include <stdio.h>
+
 #include "adapter_example.h"
 
 #include "mcu.h"
 
-static uint16_t measured_distance[DS_AMOUNT];
+__attribute__((used)) static uint16_t measured_distance[DS_AMOUNT];
 
 /*****************************************
  * Main Function
@@ -25,11 +27,18 @@ int main() {
 
     distance_sensors_adapter_init();
 
+    printf("************ START TESTS ************\r\n");
+
     for (;;) {
         distance_sensors_adapter_update();
 
         for (distance_sensor_position_t sensor_position = DS_FRONT_LEFT; sensor_position < DS_AMOUNT; sensor_position++) {
             measured_distance[(int) sensor_position] = distance_sensors_adapter_get(sensor_position);
+
+            printf("Sensor %d value: %d\r\n", sensor_position, measured_distance[(int) sensor_position]);
         }
+
+        mcu_sleep(1000);
+        printf("\r\n");
     }
 }
